eb498b4ce93017b248bf55191eb8f948b482356b
rohit <rohit@DESKTOP-P89CNGM>
2024-10-23 17:34:22 +0100

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,12 @@
+﻿/* math game requirements:
+ * 1. open with a menu prompting the user for their name, followed by choosing one of the 4 operators
+ * 
+ * 2. for each operator, two random numbers should be generated, prompting the user to solve displayed equations
+ * 
+ * 3. they should loop until the user inputs the wrong answer, with exceptions handled (putting a letter instead of a number)
+ * 
+ * 4. the divisions must result in integers only, with dividends being 0-100??(come back to that one)
+ * 
+ * 5. any games played should be stored in some kind of list, which the user can view, no need for db tho
+ */
+
